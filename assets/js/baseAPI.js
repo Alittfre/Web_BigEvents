d0e6ff21dc922c5cc8fc7a504a027bedb0f61e1f
usasaki <alittfre@gmail.com>
2023-05-27 21:44:39 +0800

    完成后台主页开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,5 +1,17 @@
 $(function () {
     $.ajaxPrefilter(function(options) {
         options.url = `http://127.0.0.1:3007${options.url}`
+        if(options.url.indexOf('/my/') !== -1) {
+            options.headers = {
+                Authorization: localStorage.getItem('BigEventToken') || ''
+            }
+            options.complete = function(res) {
+                if(res.responseJSON.status === 1) {
+                    alert('验证用户token失败')
+                    localStorage.removeItem('BigEventToken')
+                    return location.href = '../../login.html'
+                }
+            }    
+        }
     })
 })
---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$(function() {
+    getUserToken()
+    let layer = layui.layer
+    // 退出按钮功能
+    $('.layui-nav-item').click(function() {
+        layer.confirm('确认退出登录？', {icon: 3, title:'提示'}, function(index){
+            localStorage.removeItem('BigEventToken')
+            location.href = '../../login.html'
+            layer.close(index);
+          });
+    })
+})
+
+function getUserToken() {
+    $.ajax({
+        type: "GET",
+        url: "/my/userinfo",
+        success: function (response) {
+            if(response.status !== 0) {
+                return
+            }
+            getUserInfo(response.data.nickname, response.data.username, response.data.user_pic)
+        }
+    });
+}
+
+function getUserInfo(nickname, username, user_pic) {
+    let name = nickname || username
+    if(user_pic) {
+        $('.text-avatar').hide()
+        $('.layui-nav-img')
+        .attr('src', user_pic)
+        .show()
+        
+    } else {
+        $('.layui-nav-img').hide()
+        $('.text-avatar').html(name[0])
+        .show()
+    }
+
+    $('#welcome').text(`欢迎 ${name}`)
+}
+
